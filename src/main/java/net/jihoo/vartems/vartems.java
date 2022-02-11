7e76d7b7bca49f7d2113834ccfaaa2e74565c4f6
Jihuu621 <79101596+Jihuu621@users.noreply.github.com>
2022-02-11 16:31:36 +0900

    New Item

--- src/main/java/net/jihoo/vartems/vartems.java
+++ src/main/java/net/jihoo/vartems/vartems.java
@@ -1,9 +1,11 @@
 package net.jihoo.vartems;
 
+import net.jihoo.vartems.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,12 +28,11 @@ public class vartems
     public static final String MOD_ID = "vartems";
 
     public vartems() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
