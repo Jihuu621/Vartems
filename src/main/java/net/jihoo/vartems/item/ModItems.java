e0a384bffcf838aad381b30e641d18ab58eba082
Jihuu621 <79101596+Jihuu621@users.noreply.github.com>
2022-02-11 17:00:30 +0900

    New Item

--- src/main/java/net/jihoo/vartems/item/ModItems.java
+++ src/main/java/net/jihoo/vartems/item/ModItems.java
@@ -15,6 +15,8 @@ public class ModItems {
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> RAW_RUBY = ITEMS.register("raw_ruby",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 
     public static void register(IEventBus eventBus) {
